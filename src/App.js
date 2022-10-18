a245ca46763ee5f18d8eeece4a49a23aadd5b6a8
monamankar <monisk2228@gmail.com>
2022-10-18 15:44:54 +0530

    app.js modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home1 from './Home1';
 import AboutUs1 from './AboutUs1'
 import ContactUs1 from './ContactUs1'
 import Login1 from './Login1'
-import Register2 from './register2'
+import Register1 from './Register1'
 import NoPage1 from './NoPage1' 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Route path='aboutus' element={<AboutUs1/>} />
         <Route path='contactus' element={<ContactUs1/> } />
         <Route path='login' element={<Login1/>} />
-        <Route path='register' element={<Register2/>} />
+        <Route path='register' element={<Register1/>} />
         <Route path="*" element={<NoPage1 />} />
       </Routes>
    
